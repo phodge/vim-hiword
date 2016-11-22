0aa6b1ed2264bd7b8f230c0988ac3af10a4b9924
Peter Hodge <peter.hodge84@gmail.com>
2016-11-22 22:43:29 +1000

    Proper implementation using b:changedtick to prevent race conditions

--- hiword.py
+++ hiword.py
@@ -18,7 +18,116 @@ class BufferInfo(object):
         self.bufnr = bufnr
         self.lines = []
         self.locked = False
-        self.highlights = {}
+        self.pending = {}
+        self.pushing = False
+        self.pushbroken = False
+
+    def flaglines(self, firstline, numremoved, numadded):
+        # lets say that firstline is 1 (line 2), and we already have pending
+        # changes for lines 0,
+        # 1, 2, 3
+        # numremoved is 1, meaning that the line was DELETED
+        # deletebefore will be 2
+        deletebefore = firstline + numremoved
+        # numadded is 0, meaning shuffle is -1
+        shuffle = numadded - numremoved
+        # this means every linenr BEFORE 1 stays the same,
+        # every linenr >= deletebefore needs to be shuffled by -1, and anything
+        # else disappears
+
+        # SCENARIO B:
+        # firstline is 3 (line 4), numremoved is 1, numadded is 1
+        # shuffle will be 0
+        # deletebefore will be 3
+
+        newpending = list(range(firstline, firstline + numadded))
+        for linenr in self.pending:
+            if linenr < firstline:
+                # every pending line before line 1 stays the same
+                newpending.append(linenr)
+            elif linenr >= deletebefore:
+                # the pending changes for lines 2 and 3 now become pending
+                # changes for lines 1 and 2
+                newpending.append(linenr + shuffle)
+            else:
+                # the linenr falls between linenr and deletebefore, so we
+                # discard it (add it separately)
+                pass
+
+        self.pending = newpending
+
+    def pushhighlights(self, nvim):
+        # don't push highlights when we're already pushing highlights
+        if self.pushing:
+            return
+
+        # lets throw in an arbitrary sleep to give the user time to push lines
+        # down
+        #import time
+        #time.sleep(3)
+
+        try:
+            self.pushing = True
+            self._push(nvim)
+        finally:
+            self.pushing = False
+
+    def _push(self, nvim):
+        global SOURCEID
+        # if we don't have a SOURCEID, we need to get one now
+        if SOURCEID == 0:
+            SOURCEID = self.nbuf.api.add_highlight(SOURCEID, 'Error',
+                                                   0, 1, 2)
+            self.nbuf.api.clear_highlight(SOURCEID, 0, -1)
+
+        # push all pending highlights
+        while len(self.pending):
+            linenr = self.pending[0]
+            realline = linenr + 1
+
+            trytick = self.tick
+            methods = [
+                # this will fail if the ticks aren't sync'd
+                ['nvim_eval',
+                 ['b:changedtick == %d || nr2char()' % trytick]],
+                # clear highlighting for the current line
+                ['nvim_buf_clear_highlight',
+                 [self.nbuf, SOURCEID, linenr, linenr + 1]]
+            ]
+
+            # work out highlighting for this line
+            for m in MYREGEX.finditer(self.lines[linenr]):
+                start, end = m.span()
+                methods.append(['nvim_buf_add_highlight',
+                                [self.nbuf, SOURCEID, HLGROUP,
+                                 linenr, start, end]])
+
+            # SO: the things that can happen suring call_atomic() are:
+            # - another LiveUpdate is received first.
+            #   - if this happens, we're ok because the call_atomic is going to
+            #     fail
+            assert not self.pushbroken
+            retvals, errors = nvim.api.call_atomic(methods)
+
+            # if we have self.pushbroken, that means we received a LiveUpdate
+            # while sending our new HL text so we need to try and send it out
+            # again
+            if self.pushbroken:
+                self.pushbroken = False
+                continue
+
+            if errors is not None:
+                # the first expr involving b:changedtick is the only one that
+                # should fail ...
+                assert errors[0] == 0, "Only the first api call should fail"
+                # so if the b:changedtick check failed, it means there is
+                # another LiveUpdate on the way with new buffer changes, so we
+                # stop trying to push changes and wait for the next LiveUpdate
+                return
+
+            # b:changedtick hadn't changed, so we can be certain that our
+            # HL push was done correctly, THEREFORE we can pop this line
+            self.pending.pop(0)
 
 
 def removehighlights(info, start, num):
@@ -74,7 +183,8 @@ def handle_request(nvim, name, args):
         try:
             buffer.api.live_updates(True)
         except:
-            raise Exception("Couldn't turn on live updates for buffer %d" % bufnr)
+            raise Exception("Couldn't turn on live updates for buffer %d"
+                            % bufnr)
         BUFFERS[bufnr] = BufferInfo(buffer, bufnr)
         return
 
@@ -84,11 +194,12 @@ def handle_request(nvim, name, args):
 def handle_notification(nvim, name, args):
     try:
         if name == 'LiveUpdateStart':
-            buf, lines, more = args
+            buf, tick, lines, more = args
             bufnr = buf.number
             assert bufnr in BUFFERS
             info = BUFFERS[bufnr]
             assert not info.locked
+            info.tick = tick
             info.lines += lines
             if not more:
                 info.locked = True
@@ -96,29 +207,41 @@ def handle_notification(nvim, name, args):
                 addhighlights(info)
             return
 
-        if name == 'LiveUpdate':
-            buf, firstline, numreplaced, linedata = args
+        if name == 'LiveUpdateTick':
+            buf, tick = args
+            assert tick is not None
             bufnr = buf.number
             info = BUFFERS[bufnr]
-            # remove existing highlights on the lines that were changed
-            if numreplaced and len(linedata):
-                removehighlights(info, firstline, len(linedata))
-            # swap in the new lines
+            info.tick = tick
+            # if we are part-way through pushing a HL change, mark it as broken
+            if info.pushing:
+                info.pushbroken = True
+            return
+
+        if name == 'LiveUpdate':
+            buf, tick, firstline, numreplaced, linedata = args
+            bufnr = buf.number
+            info = BUFFERS[bufnr]
+            # NOTE: 'inccommand' will cause a LiveUpdate to come through with
+            # b:changedtick of None ... this is because it updates the text
+            # which is displayed on screen, but doesn't actually update the
+            # buffer contents
+            if tick is not None:
+                info.tick = tick
+            # swap in the new line data
             info.lines[firstline:firstline+numreplaced] = linedata
-            if len(linedata):
-                addhighlights(info, firstline, len(linedata))
+            if info.pushing:
+                info.pushbroken = True
+            info.flaglines(firstline, numreplaced, len(linedata))
+            if not info.pushing:
+                info.pushhighlights(nvim)
+            #if len(linedata):
+                #addhighlights(info, firstline, len(linedata))
             return
 
         if name == 'LiveUpdateEnd':
-            buf = args[0]
-            bufnr = buf.number
-            # try and re-add the live updates
-            try:
-                buffer = BUFFERS[bufnr]
-            except:
-                return
-            if not buffer.nbuf.api.live_updates(True):
-                BUFFERS.pop(bufnr)
+            bufnr = args[0].number
+            BUFFERS.pop(bufnr, None)
             return
 
         raise Exception("Unexpected event {!r}: {!r}".format(name, args))
